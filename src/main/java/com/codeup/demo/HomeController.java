ee962e5b55cf9b6513d7e086cd7c3eb17d2d5311
Jason Carrillo <jason.carrillo182@gmail.com>
2020-11-24 12:57:54 -0700

    post method works in join page

--- src/main/java/com/codeup/demo/HomeController.java
+++ src/main/java/com/codeup/demo/HomeController.java
@@ -1,10 +1,8 @@
 package com.codeup.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -13,4 +11,39 @@ public class HomeController {
     public String sayHello() {
         return "This is the landing page!";
     }
+
+    @GetMapping("/home")
+    public String welcome(){
+        return "/home";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "/hello";
+    }
+
+    @GetMapping("/create-a-hello")
+    @ResponseBody
+    public String showCreate(){
+        return "create form";
+    }
+
+    @GetMapping("/hello/{name}") //request
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "/hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "/join";
+    }
+
+    @PostMapping("/join")
+    public String postJoinForm(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "/join";
+    }
+
+
 }
