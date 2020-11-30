ab817d21826530f32bf91c92b1d94c245744d9d0
Jason Carrillo <jason.carrillo182@gmail.com>
2020-11-30 09:00:27 -0700

    created contructor getters, and setters

---
+++ src/main/java/Post.java
@@ -0,0 +1,29 @@
+public class Post {
+    private String title;
+    private String body;
+
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setBody(String body){
+        this.title = body;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getBody(){
+        return this.body;
+    }
+
+
+
+}
