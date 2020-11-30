51c1cfdc0e3b07b20890a245210bf7dc0b042e8f
Jason Carrillo <jason.carrillo182@gmail.com>
2020-11-30 15:47:20 -0700

    added ad class constructor getters, and setters for title desc, and id

---
+++ src/main/java/Ad.java
@@ -0,0 +1,50 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ads")
+public class Ad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    public Ad(){}
+
+
+
+    public Ad(long id, String title, String description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
